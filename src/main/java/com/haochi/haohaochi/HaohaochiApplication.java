4a673bb5e5cccb6e132e368c2f4d3da9fc783b2d
用户名 <1262967980@qq.com>
2019-02-14 19:37:40 +0800

    架子

---
+++ src/main/java/com/haochi/haohaochi/HaohaochiApplication.java
@@ -0,0 +1,14 @@
+package com.haochi.haohaochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaohaochiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaohaochiApplication.class, args);
+    }
+
+}
+
